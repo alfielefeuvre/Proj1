34f29157527c770b0a080e0633a383598a9fad03
Alfie Le Feuvre <alfielefeuvre@icloud.com>
2024-05-02 18:58:28 +0100

    Initial Commit

---
+++ Proj1 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proj1 Watch App
+//
+//  Created by Alfie Le Feuvre on 02/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!2")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proj1 Watch App/Proj1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proj1App.swift
+//  Proj1 Watch App
+//
+//  Created by Alfie Le Feuvre on 02/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Proj1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
